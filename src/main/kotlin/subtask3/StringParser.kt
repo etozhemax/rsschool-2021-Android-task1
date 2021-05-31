f229624454c7ce608a8de284d69114a2fb76da91
Baranovsky Maxim <baranowski.maxim@gmail.com>
2021-05-31 16:56:06 +0300

    feat complete task 3

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,36 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var resultList = mutableListOf<String>()
+
+        for ((index, element) in inputString.withIndex())
+        {
+            var isSkipped = false
+            if (inputChars.keys.contains(element)) {
+
+                for (innerIndex in (index + 1) until inputString.length) {
+                    if (inputString[innerIndex] == element) {
+                        isSkipped = true
+                    }
+                    if ((inputString[innerIndex] == inputChars[element])) {
+                        if (isSkipped) {
+                            isSkipped = false
+                            continue
+                        }
+
+                        resultList.add(inputString.substring(index + 1, innerIndex))
+                        break
+                    }
+                }
+
+
+            }
+        }
+
+        return resultList.toTypedArray()
+    }
+
+    companion object {
+        val inputChars = mapOf<Char, Char>('<' to '>', '[' to ']', '(' to ')')
     }
 }
