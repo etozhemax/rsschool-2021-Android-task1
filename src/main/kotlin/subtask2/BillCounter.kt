8d4e4819ba57a711912c35dbc8d4b0783dfe7dae
Baranovsky Maxim <baranowski.maxim@gmail.com>
2021-05-31 15:20:10 +0300

    feat complete task 2

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,16 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        var sumToPay = (bill.sum() - bill[k]) / 2;
+
+        if (sumToPay == b) {
+            return BonAppetit
+        } else {
+            return (b - sumToPay).toString()
+        }
+    }
+
+    companion object {
+        const val BonAppetit = "Bon Appetit"
     }
 }
